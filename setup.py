bf585700cb578cd4fae723a479adf1be90264043
StarostinV <vladimir.starostin@uni-tuebingen.de>
2020-03-12 18:23:01 +0100

    add long description to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, Extension
+from pathlib import Path
 
 import numpy
 
 USE_CYTHON = False
-VERSION = '1.0.0'
+VERSION = '1.0.1'
+
+
+def read(filename: str):
+    with open(Path(__file__).parent / filename, mode='r', encoding='utf-8') as f:
+        return f.read()
+
 
 if USE_CYTHON:
     try:
@@ -18,12 +25,11 @@ ext = '.pyx' if USE_CYTHON else '.c'
 
 include_dirs = []
 extensions = [Extension('box_interpolation', [f'box_interpolation{ext}'],
-              include_dirs=[numpy.get_include()])]
+                        include_dirs=[numpy.get_include()])]
 
 if USE_CYTHON:
     extensions = cythonize(extensions)
     include_dirs = [numpy.get_include()]
-
 
 setup(
     name='box_interpolation',
@@ -31,12 +37,16 @@ setup(
     author='Vladimir Starostin',
     author_email='vladimir.starostin@uni-tuebingen.de',
     description='Simple fast box interpolation powered by Cython.',
-    # long_description_content_type='text/markdown',
+    long_description_content_type='text/markdown',
     license='MIT',
+    long_description=read('README.md'),
     ext_modules=extensions,
     include_dirs=include_dirs,
+    include_package_data=True,
     python_requires='>=3.6.*',
     install_requires=[
         'numpy>=1.18.1',
     ],
+    keywords='non-grid interpolation box_interpolation cython',
+    url='https://pypi.org/project/box_interpolation/',
 )
